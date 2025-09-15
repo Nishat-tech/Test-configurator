d6cd1c150f574f6d018ebd4f4d0c974502d15d7c
Your Name <nishatkanwal21@gmail.com>
2025-09-15 17:09:34 +0500

    fule update

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 const environment = {
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/santa_monika',
-  JWT_SECRET: process.env.JWT_SECRET || 'your-secret-key',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://Tester_db_user:<db_password>@santamonika.7aqm5oo.mongodb.net/?retryWrites=true&w=majority&appName=SantaMonika',
+  JWT_SECRET: process.env.JWT_SECRET || 'DDCB940505B1349DB5FD5D8E4F1A8A6B13AF81E42A4AAE0D76ED237CC8DCD52C',
   PORT: parseInt(process.env.PORT, 10) || 5000,
   NODE_ENV: process.env.NODE_ENV || 'development'
 };
